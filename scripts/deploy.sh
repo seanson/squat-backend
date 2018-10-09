96b48366f58e209c6727fc1ad5aba6672c7ad3df
Sean Johnson <seanson@users.noreply.github.com>
2018-10-09 21:10:28 +1100

    Change build script to deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ CLUSTER=squat-cluster
 ZONE=us-central1-a
 DOCKER_IMAGE=squat-backend
 
-set -e
+set -euo pipefail
 
 docker build -t gcr.io/${PROJECT}/${DOCKER_IMAGE}:$TRAVIS_COMMIT .
 
@@ -15,10 +15,20 @@ gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
 gcloud --quiet config set project ${PROJECT}
 gcloud --quiet config set container/cluster ${CLUSTER}
 gcloud --quiet config set compute/zone ${ZONE}
-# gcloud --quiet container clusters get-credentials ${CLUSTER}
+gcloud --quiet container clusters get-credentials ${CLUSTER}
 
 gcloud auth configure-docker
 
 docker push gcr.io/${PROJECT}/${DOCKER_IMAGE}:${TRAVIS_COMMIT}
 
+helm template --set global.version=${TRAVIS_COMMIT} \
+              charts/deploy | \
+              kubectl apply -f -
+
+kubectl wait --namespace ${DOCKER_IMAGE} \
+             --for=condition=available \
+             --timeout=60s \
+             deployment/${DOCKER_IMAGE}
+
 yes | gcloud beta container images add-tag gcr.io/${PROJECT}/${DOCKER_IMAGE}:${TRAVIS_COMMIT} gcr.io/${PROJECT}/${DOCKER_IMAGE}:latest
+
